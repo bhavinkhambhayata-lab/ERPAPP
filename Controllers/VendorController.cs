8202383feace5ab2b19163e0e97056a224fe34ae
BhavinKhambhayata <BhavinKhambhayata@ITALIA_BHAVIN>
2026-03-19 12:39:56 +0530

    vendor controller add and partial view add only and create sp GetVendorAddDropDownData

---
+++ Controllers/VendorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERPAPP.Controllers
+{
+    public class VendorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult VendorList()
+        {
+            return PartialView("_VendorList");
+        }
+
+        public IActionResult VendorDetails()
+        {
+            return PartialView("_VendorDetails");
+        }
+
+    }
+}
