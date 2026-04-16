45fcf5c1a62a2caa5f2f04d863faaab59680d409
BhavinKhambhayata <BhavinKhambhayata@ITALIA_BHAVIN>
2026-04-16 18:19:07 +0530

    small changes

--- wwwroot/js/Vendor/edit-unblock-vendor.js
+++ wwwroot/js/Vendor/edit-unblock-vendor.js
@@ -630,3 +630,35 @@ function loadVendorNonEditableFields() {
         $("#IFSCCode").prop("disabled", true);
     }
 }
+
+function toggleVendorUnBlock(btn) {
+
+    var vendorCode = btn.getAttribute("data-vendor");
+
+    if (!confirm("Are you sure you want to unblock this vendor?")) {
+        return; // ❌ cancel
+    }
+
+    $.ajax({
+        url: baseURL + 'Vendor/VendorUnblock',
+        type: 'POST',
+        data: {
+            vendorCode: vendorCode,
+            displayRowId: $("#DisplayNo").text(),
+            vendorName: $("#Name").val(),
+            locationName: $("#Location").val()
+        },
+        success: function (res) {
+
+            if (res.success) {
+
+                showToast(res.message, "success", 3000);
+
+                btn.style.display = "none";
+
+            } else {
+                alert(res.message || "Something went wrong!");
+            }
+        }
+    });
+}
